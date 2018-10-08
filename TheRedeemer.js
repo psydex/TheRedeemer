a378deb74d552211c08a4c53e184e1a847664e77
psydex <psydex@mobers.org>
2018-10-08 16:02:42 +0300

    v2.0
    This is version two which automatically pastes the already copied key into the Steam activation page.

---
+++ TheRedeemer.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        The Redeemer
+// @namespace   raina (modified by psydex)
+// @description Pops up the Steam product activation webpage when copying keys from any website. Does not require Steam to be installed!
+// @include     *
+// @version     2.0
+// @grant       none
+// ==/UserScript==
+(function() {
+	"use strict";
+	var activateProduct = function(e) {
+		var productKey = window.getSelection().toString().trim() || e.target.value;
+		if (/^[\d\w]{2,5}(\-[\d\w]{4,5}){2,4}$/.test(productKey)) {
+			window.open("https://store.steampowered.com/account/registerkey?key=" + productKey);
+		}
+	};
+	window.addEventListener("copy", activateProduct, false);
+}());
